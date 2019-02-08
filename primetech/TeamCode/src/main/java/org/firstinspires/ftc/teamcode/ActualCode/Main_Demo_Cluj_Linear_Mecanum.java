8a109412f300d236dbe7f3d72e783257f4da6b81
anisca22 <anisca22@yahoo.com>
2019-02-08 22:44:10 +0200

    Mecanum - the beginning
    Added Hardware Mecanum which basically sets the direction to all the motors to be the same.
    Added Main_Demo_Cluj_Linear_Mecanum with the working mecanum drive code included.

---
+++ primetech/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ActualCode/Main_Demo_Cluj_Linear_Mecanum.java
@@ -0,0 +1,201 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.ActualCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.HardwareDemoCluj;
+import org.firstinspires.ftc.teamcode.HardwareMecanum;
+
+import static org.firstinspires.ftc.teamcode.HardwareDemoCluj.COUNTS_PER_CM;
+import static org.firstinspires.ftc.teamcode.HardwareDemoCluj.LOCK_CLOSED;
+import static org.firstinspires.ftc.teamcode.HardwareDemoCluj.LOCK_OPEN;
+import static org.firstinspires.ftc.teamcode.HardwareDemoCluj.MARKER_RELEASED;
+import static org.firstinspires.ftc.teamcode.HardwareDemoCluj.MARKER_START;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Cluj_Linear_Mecanum", group="Linear Opmode")
+//@Disabled
+public class Main_Demo_Cluj_Linear_Mecanum extends LinearOpMode {
+
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    double armPower = 0;
+    double markerPosition = MARKER_START;
+    double lockPosition = LOCK_CLOSED;
+    double leftPower = 0;
+    double rightPower = 0;
+    double drive = 0;
+    double turn = 0;
+    double strafe = 0;
+    double frontLeftPower = 0;
+    double frontRightPower = 0;
+    double backLeftPower = 0;
+    double backRightPower = 0;
+    HardwareMecanum robot = new HardwareMecanum();
+
+    @Override
+    public void runOpMode() {
+        robot.init(hardwareMap);
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            /**GAMEPAD 1**/
+
+            drive = -gamepad1.left_stick_y;
+            strafe = gamepad1.left_stick_x;
+            turn = gamepad1.right_stick_x;
+
+            frontLeftPower  = Range.clip(+drive - turn + strafe, -1.0, 1.0);
+            frontRightPower = Range.clip(-drive - turn + strafe, -1.0, 1.0);
+            backLeftPower   = Range.clip(+drive - turn - strafe, -1.0, 1.0);
+            backRightPower  = Range.clip(-drive - turn - strafe, -1.0, 1.0);
+
+            //LOCK SERVO
+            if (gamepad1.x)
+                lockPosition = LOCK_OPEN;
+            else if (gamepad1.y)
+                lockPosition = LOCK_CLOSED;
+
+            /**GAMEPAD 2**/
+
+            //MARKER SERVO
+            if (gamepad1.a)
+                markerPosition = MARKER_RELEASED;
+            else if (gamepad1.b)
+                markerPosition = MARKER_START;
+
+            //RIDICARE BRAT MANUAL
+            if (gamepad1.dpad_up)
+                armPower = 1;
+            else if (gamepad1.dpad_down)
+                armPower = -1;
+            else if (!(gamepad1.dpad_up ||gamepad1.dpad_down))
+                armPower = 0;
+
+            robot.armMotor.setPower(armPower);
+
+            // PUTERE LA MOTOARE DE DEPLASARE
+            robot.backLeftMotor.setPower(backLeftPower);
+            robot.frontLeftMotor.setPower(frontLeftPower);
+            robot.backRightMotor.setPower(backRightPower);
+            robot.frontRightMotor.setPower(frontRightPower);
+
+            //POZITIONARE SERVOURI
+            robot.markerServo.setPosition(markerPosition);
+            robot.lockServo.setPosition(lockPosition);
+
+        }
+
+        stopItDude();
+
+    }
+
+    public void stopItDude(){
+        //OPRIT MOTOARE DE DEPLASARE
+        robot.backLeftMotor.setPower(0);
+        robot.frontLeftMotor.setPower(0);
+        robot.backRightMotor.setPower(0);
+        robot.frontRightMotor.setPower(0);
+
+        //OPRIT MOTORUL DE RIDICARE
+        robot.armMotor.setPower(0);
+
+        //SETARE POZITIE SERVOURI DE SFARSIT
+        robot.lockServo.setPosition(LOCK_CLOSED);
+        robot.markerServo.setPosition(MARKER_START);
+    }
+
+    public void encoderArm(double speed, double distance, double direction, double timeoutS) {
+        int armTarget;
+
+        if (direction == -1)
+            robot.armMotor.setDirection(DcMotor.Direction.REVERSE);
+        else if (direction == 1)
+            robot.armMotor.setDirection(DcMotor.Direction.FORWARD);
+        // Ensure that the opmode is still active
+        robot.armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        if (opModeIsActive() && !isStopRequested() && !(gamepad1.dpad_up && gamepad1.b)) {
+
+            // Determine new target position, and pass to motor controller
+            armTarget = robot.armMotor.getCurrentPosition() + (int)(distance * COUNTS_PER_CM);
+
+            robot.armMotor.setTargetPosition(armTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.armMotor.setPower(Math.abs(speed));
+
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (robot.armMotor.isBusy())
+                    && !isStopRequested() && !(gamepad1.a && gamepad1.b))
+            {
+
+                // Display it for the driver.
+                telemetry.addData("Path1",  "Running to :%7d", armTarget);
+                telemetry.addData("Path2",  "Running at :%7d", robot.armMotor.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stop all motion;
+            robot.armMotor.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            robot.armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.armMotor.setDirection(DcMotor.Direction.FORWARD);
+        }
+    }
+}
---
+++ primetech/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMecanum.java
@@ -0,0 +1,134 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a K9 robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Servo channel:  Servo to raise/lower arm: "arm"
+ * Servo channel:  Servo to open/close claw: "claw"
+ *
+ * Note: the configuration of the servos is such that:
+ *   As the arm servo approaches 0, the arm position moves up (away from the floor).
+ *   As the claw servo approaches 0, the claw opens up (drops the game element).
+ */
+public class HardwareMecanum
+{
+    ///DECLARARE MOTOARE
+    public DcMotor  frontLeftMotor;
+    public DcMotor  backLeftMotor;
+    public DcMotor  frontRightMotor;
+    public DcMotor  backRightMotor;
+
+    public DcMotor  armMotor;
+
+    ///DECLARARE SERVOURI
+    public Servo markerServo;
+    public Servo lockServo;
+
+    public final static double MARKER_START = 1;
+    public final static double MARKER_RELEASED = 0.5;
+
+    public final static double LOCK_OPEN = 0.2;
+    public final static double LOCK_CLOSED = 0;
+
+    public static final double     BIG_TURN_SPEED            = 0.4;
+    public static final double     TURN_SPEED            = 0.15;
+    public static final double     DRIVE_SPEED            = 0.5;
+    public static final double     HEADING_THRESHOLD       = 10;
+    public static final double     P_TURN_COEFF            = 0.1;
+    public static final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // eg: TETRIX Motor Encoder 1440 tetrix
+    public static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    public static final double     WHEEL_DIAMETER_CM   = 4.0 * 2.54;     // For figuring circumference
+    public static final double     COUNTS_PER_CM         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_CM * 3.1415);
+
+    /* Local OpMode members. */
+    HardwareMap hwMap  = null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public HardwareMecanum() {}
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // save reference to HW Map
+        hwMap = ahwMap;
+
+
+        // DEFINIRE MOTOARE
+        frontLeftMotor = hwMap.get(DcMotor.class, "frontLeftMotor");
+        backLeftMotor = hwMap.get(DcMotor.class, "backLeftMotor");
+        frontRightMotor = hwMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hwMap.get(DcMotor.class, "backRightMotor");
+
+        armMotor = hwMap.get(DcMotor.class, "armMotor");
+
+        //DEFINIRE SERVOURI
+        lockServo = hwMap.get (Servo.class, "lock");
+        markerServo = hwMap.get (Servo.class, "marker");
+
+        // SETARE DIRECTIE
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        // PUTERE 0
+        frontLeftMotor.setPower(0);
+        backLeftMotor.setPower(0);
+        frontRightMotor.setPower(0);
+        backRightMotor.setPower(0);
+
+        armMotor.setPower(0);
+
+        // SETARE POZITIE SERVO
+        lockServo.setPosition(LOCK_CLOSED);
+        markerServo.setPosition(MARKER_START);
+
+    }
+
+}
